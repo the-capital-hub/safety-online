a0e35c4460f0c787ccd0b50ddaa0d06372a64382
NitinJajoria <95345434+NitinJajoria@users.noreply.github.com>
2025-08-20 16:33:20 +0530

    minor change

--- app/page.js
+++ app/page.js
@@ -1,17 +1,7 @@
-"use client";
-
-import React, { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 const Home = () => {
-	const router = useRouter();
-
-	// On page mount redirect to /home
-	useEffect(() => {
-		router.push("/home");
-	});
-
-	return <div>Redirecting to home page</div>;
+	redirect("/home");
 };
 
 export default Home;
